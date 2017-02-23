c27887c434021246f1c0853cf2dc64dd5b3172e5
bsenior1 <bsenior@clemson.edu>
2017-02-23 15:40:09 -0800

    ExtremeNodeJSApp Update

--- app.js
+++ app.js
@@ -7,6 +7,9 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+	request = require('request'),
+	
+	_= require('lodash');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -21,6 +24,23 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+app.get('/process_get', function(req, res) {
+	// prepare output in json format
+	response = {
+		latitude:req.query.latitude,
+		longitude:req.query.longitude
+	};
+	
+	var callURL = "https://8d06e217-8e7e-49ca-91d9-8f1dd6f85d88:TAao24v59K@twcservice.mybluemix.net/api/weather/v1/geocode/"+response.latitude+"/"+response.longitude+"/forecast/hourly/48hour.json?units=m&language=en-US";
+	
+	request.get(callURL, {
+		json: true
+	},
+	function (error, response, body) {
+		console.log(body)
+	});
+	
+})
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
