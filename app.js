4454db7adbc033db70c35a0db5bc84107d3828f7
bsenior1 <bsenior@clemson.edu>
2017-02-28 16:32:52 -0800

    Update: app.js

--- app.js
+++ app.js
@@ -37,8 +37,7 @@ app.get('/process_get', function(req, res) {
 		json: true
 	},
 	function (error, response, body) {
-		console.log(body),
-		object = JSON.parse(body),
+		var object = JSON.parse(body),
 		console.log(object.num)
 	});
 	
