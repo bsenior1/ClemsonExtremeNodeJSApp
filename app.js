7f66115c5d40a3a10fc1e790269f8a92a10de9a8
bsenior1 <bsenior@clemson.edu>
2017-03-07 11:23:53 -0800

    Update: app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function getWeather(){
 		json: true
 	},
 	function (error, response, body) {
-		console.log(metadataCount + " - The Parsed MetaData: ", body.forecasts);
+		console.log(metadataCount + " - The Parsed MetaData: ", body.forecasts[0]);
 		//res.end(JSON.stringify(body.metadata));
 	});
 	
