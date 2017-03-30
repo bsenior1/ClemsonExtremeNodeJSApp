4a39ce19624f0abb90b148465c0ef5dbaf19c9c8
bsenior1 <bsenior@clemson.edu>
2017-03-30 14:49:41 -0700

    weather.js and app.js update

--- app.js
+++ app.js
@@ -1,45 +1,11 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
+// Dependencies
 var express = require('express');
 var request = require('request');
-var Twitter = require('twitter');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
-var parser = require('json-parser');
-
-var Client = require("ibmiotf");
-var config = {
-    "org" : "wkabcg",
-    "id" : "deviceid1",
-    "domain": "internetofthings.ibmcloud.com",
-    "type" : "Device1",
-    "auth-method" : "token",
-    "auth-token" : "PY&Lzd105iT7QuRhg&"
-};
-var deviceClient = new Client.IotfDevice(config);
-deviceClient.connect();
-
-var twitterClient = new Twitter({
-        consumer_key: 'uyGpIwSq2QmEjok8qsvAtwCW0',
-        consumer_secret: 'P1Jona5WwUE54WF8LjQHE8ZJa2zK7wKmLGTj8WPZyGHqeMhxHl',
-        access_token_key: '132337921-mWzwmPzlpWS9m5B7pw7T3VIWIgkPQQYUVkuGJqTG',
-        access_token_secret: 'waoDLEZrg14hV2dox0FwTUs4Qv5C3ReajWOtVnlLVigUM'
-    });
+var weatherVar = require('./weather.js');
 
 // create a new express server
 var app = express();
-
-var timesGetWeatherCalled = 0;
-var weatherIntervalID;
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -47,19 +13,8 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// When the device connects
-deviceClient.on("connect", function() {
-	console.log("Device connected!");
-});
-
-// When the device receives an error
-deviceClient.on("error", function(err) {
-	console.log("Error! - " + err);
-});
-
 app.get('/process_get', function(req, res)
 {
-	timesGetWeatherCalled = 0;
 	// Prepare output in JSON format
 	response = {
 		lat1: req.query.latitude1,
@@ -67,48 +22,13 @@ app.get('/process_get', function(req, res)
 		lat2: req.query.latitude2,
 		long2: req.query.longitude2
 	};
-	
+
+    timesGetWeatherCalled = 0;	
 	weatherIntervalID = setInterval(function() {
 		getWeather();
 	}, 10000);
-
-	var locationString = "";
-	locationString += response.lat1 + "," + response.long1 + "," + response.lat2 + "," + response.long2;
-	var stream = twitterClient.stream("statuses/filter", { locations: locationString });
-	stream.on("data", function(event) {
-		console.log(event && event.text);
-		deviceClient.publish("status", "json", '{"d": {"text": ' + event.text + '}}');
-	});
-	// res.setHeader("Content-Type", "text/html");
-	// res.end("<form action='https://jpeterkdemoapp.mybluemix.net/process_get' method='GET'>" + 
- //      		"Latitude: <input type='text' name='latitude' /><br />" + 
- //      		"Longitude: <input type='text' name='longitude' /><br />" +
- //      		"<input type='submit' text='submit' />" +
- //    		"</form>" +
- //    		"<p id='blankSpace'>" + JSON.stringify(body.forecasts) + "</p>");
- //
 });
 
-function getWeather()
-{
-	if (timesGetWeatherCalled >= 5)
-	{
-		clearInterval(weatherIntervalID);
-		deviceClient.disconnect();
-	}
-	else
-	{
-		var callURL = "https://8d06e217-8e7e-49ca-91d9-8f1dd6f85d88:TAao24v59K@twcservice.mybluemix.net/api/weather/v1/geocode/" + response.lat1 + "/" + response.long1 + "/forecast/hourly/48hour.json?units=m&language=en-US";
-		request.get(callURL, {
-			json: true
-		},
-		function (error, response, body) {
-			console.log("forecast: " + body.forecasts);
-			deviceClient.publish("status", "json", JSON.stringify(body.forecasts));
-		});
-	}
-	timesGetWeatherCalled++;
-}
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
---
+++ weather.js
@@ -0,0 +1,54 @@
+//Dependencies
+var Client = require("ibmiotf");
+
+// Variables for Limiting the Quantity of Weather Based Information Responses
+var timesGetWeatherCalled = 0;
+var weatherIntervalID = 0;
+
+// IOT Device Configuration and Connection
+var config = {
+    "org" : "wkabcg",
+    "id" : "deviceid1",
+    "domain": "internetofthings.ibmcloud.com",
+    "type" : "Device1",
+    "auth-method" : "token",
+    "auth-token" : "PY&Lzd105iT7QuRhg&"
+};
+var deviceClient = new Client.IotfDevice(config);
+deviceClient.connect();
+
+
+// When the device connects
+deviceClient.on("connect", function() {
+	console.log("Device connected!");
+});
+
+// When the device receives an error
+deviceClient.on("error", function(err) {
+	console.log("Error! - " + err);
+});
+
+
+// Function to publish forecast information to IOT Device
+function getWeather()
+{
+	if (timesGetWeatherCalled >= 5)
+	{
+		clearInterval(weatherIntervalID);
+		deviceClient.disconnect();
+	}
+	else
+	{
+		var callURL = "https://8d06e217-8e7e-49ca-91d9-8f1dd6f85d88:TAao24v59K@twcservice.mybluemix.net/api/weather/v1/geocode/" + response.lat1 + "/" + response.long1 + "/forecast/hourly/48hour.json?units=m&language=en-US";
+		request.get(callURL, {
+			json: true
+		},
+		function (error, response, body) {
+			console.log("forecast: " + body.forecasts);
+			deviceClient.publish("status", "json", JSON.stringify(body.forecasts));
+		});
+	}
+	++timesGetWeatherCalled;
+}
+
+
