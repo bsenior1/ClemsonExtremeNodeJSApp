0a65d30f818fd55847e28401d47559069ab15b41
bsenior1 <bsenior@clemson.edu>
2017-04-06 10:39:26 -0700

    traffic.js

--- traffic.js
+++ traffic.js
@@ -27,13 +27,13 @@ var trafficSetup = function() {
 			
 			console.log("TRAFFIC!!!");
 			var trafficURL = "http://dev.virtualearth.net/REST/v1/Traffic/Incidents/"+locationString+"?key=Akl3RNr5drLME8fYbDEpoi--QuIbaK3aIgFZR7oycJ5TdY12QYZMs4D81I9TgzEX"
-			//request.get(trafficURL, {
-				//json: true
-			//},
-			//function (error, response, body) {
-				console.log("traffic: " + body.traffic);
+			request.get(trafficURL, {
+				json: true
+			},
+			function (error, response, body) {
+				console.log("traffic: " + body);
 				//deviceClient.publish("status", "json", JSON.stringify(body.traffic));
-			//});	
+			});	
 		}
 		++timesGetTrafficCalled;
 	}
