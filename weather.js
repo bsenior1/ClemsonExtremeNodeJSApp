00c39b020d52a39eab4d71fcf6b40a5bff22bd2f
bsenior1 <bsenior@clemson.edu>
2017-04-11 08:33:23 -0700

    weather.js update

--- weather.js
+++ weather.js
@@ -20,7 +20,7 @@ var weatherSetup = function(deviceClient) {
 		else
 		{
 			console.log("WEATHER!!!");
-			var callURL = "https://8d06e217-8e7e-49ca-91d9-8f1dd6f85d88:TAao24v59K@twcservice.mybluemix.net/api/weather/v1/geocode/" + response.lat1 + "/" + response.long1 + "/forecast/hourly/48hour.json?units=m&language=en-US";
+			var callURL = process.env.WEATHER_CALL_URL + "/api/weather/v1/geocode/" + response.lat1 + "/" + response.long1 + "/forecast/hourly/48hour.json?units=m&language=en-US";
 			request.get(callURL, {
 				json: true
 			},
